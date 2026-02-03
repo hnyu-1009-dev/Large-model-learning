84931608a7d11ec2b211e5444286105d80e8ab49
Haonan Yu <hnyu1009@163.com>
2026-02-03 08:56:38 +0800

    Default

---
+++ LangChain-tutorial/chapter01-summary/Test-First.py
@@ -0,0 +1,8 @@
+import langchain
+print(langchain.__version__) # 0.3.25
+import openai
+print(openai.__version__) # 1.81.0
+
+import sys
+#查看python的版本
+print(sys.version) # 3.10.17 | packaged by conda-forge | (main, Apr 10 2025, 22:06:35) [MSCv.1943 64 bit (AMD64)]
